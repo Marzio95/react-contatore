5adf4f052d86aa9d38ae05b51d40d4084141107d
Marzio95 <marziodellarocca14@gmail.com>
2022-08-04 16:46:21 +0200

    implmento un contatore redux con tutti i suoi componenti

--- src/components/ContatoreRedux.js
+++ src/components/ContatoreRedux.js
@@ -1,5 +1,19 @@
+import { useSelector, useDispatch } from "react-redux";
+import { inc } from "../slices/counterSlice"; 
+
 export function ContatoreRedux() {
+
+    const contatore = useSelector(state => state.counter.contatore) // counter è il nome del reducer in store.js
+
+    const dispatch = useDispatch();
+
     return (
-        <h2>ContatoreRedux</h2>
+        <div>
+            <h2>ContatoreRedux</h2>
+            <h1>{contatore}</h1>
+            <button onClick={ () => dispatch( inc() ) }>+</button>
+            {/* <button onClick={dec}>-</button>
+            <button onClick={random}>Random</button> */}
+        </div>
     )
 }
--- src/slices/counterSlice.js
+++ src/slices/counterSlice.js
@@ -1,5 +1,20 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    contatore: 0
+};
 
 export const counterSlice = createSlice({
-    name: "counter",
-})
+    name: "counter", // questo è il nome che apparirà nel ReduxDevTools del browser
+    initialState,
+    reducers: {
+        inc: (state) => { // state rappresenta lo stato attuale del componente (INITIAL_STATE)
+            state.contatore = state.contatore + 1;
+        }
+    }
+});
+
+export const { inc } = counterSlice.actions; // actions è un oggetto che contiene tutte le funzioni per le azioni
+                                            // con il destructuring posso passsare un solo parametro dell'oggetto, in questo caso inc
+
+export default counterSlice.reducer;
